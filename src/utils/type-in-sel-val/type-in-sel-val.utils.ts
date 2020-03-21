608ddc158c9c29e4ced6bbde618476306419daa7
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-03-20 21:46:10 -0400

    Improve typing in `typeInSelVal`

--- src/utils/type-in-sel-val/type-in-sel-val.utils.ts
+++ src/utils/type-in-sel-val/type-in-sel-val.utils.ts
@@ -1,4 +1,4 @@
-import { Page } from 'puppeteer';
+import { Page, Frame } from 'puppeteer';
 const chalk = require('chalk')
 
 import { clickInSel } from '../';
@@ -7,7 +7,7 @@ import { log } from '../../internals/logger';
 // TODO: refactor to parameters be a config object
 // TODO: add better logging
 
-const typeInSelVal = async (page: Page, sel: string, val: string, name = '') =>  {
+const typeInSelVal = async (page: Page | Frame, sel: string, val: string, name = '') =>  {
   await clickInSel(page, sel);
   log.debug(chalk`Input Name: {green ${name}} | Value: {green ${val}}`);
   await page.type(sel, val, { delay: 111 });
