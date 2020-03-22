2e4f8fb29f49a96dc39ce23c06fd4fb1d7551c0f
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-03-22 18:01:31 -0400

    Add logs in waitForFrame

--- src/utils/wait-for-frame/wait-for-frame.utils.ts
+++ src/utils/wait-for-frame/wait-for-frame.utils.ts
@@ -1,11 +1,17 @@
 import { Frame } from 'puppeteer';
+const chalk = require('chalk');
+
+import { log } from '../../internals/logger';
 
 const waitForFrame = (page: any, name: string): Promise<Frame> => new Promise((resolve) => {
+  log.debug(chalk`{gray Waiting for frame}`);
   const checkFrame = () => {
     const frame = page.frames().find((f: any) => f.name() === name);
     if (frame) {
+      log.debug(chalk`{gray Waiting for frame} {green [ok]}`);
       resolve(frame);
     } else {
+      log.debug(chalk`{gray Frame attached - retrying... Waiting for frame}`);
       page.once('frameattached', checkFrame);
     }
   }
