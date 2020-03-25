f21bc4357e105eb4fe476475936f9208952075ae
Miguel Padrino <mikehathsin@gmail.com>
2020-03-25 18:57:56 -0400

    partial refactor: clickInSel

--- src/utils/click-in-sel/click-in-sel.utils.ts
+++ src/utils/click-in-sel/click-in-sel.utils.ts
@@ -1,8 +1,8 @@
 import { Page, Frame } from 'puppeteer';
-const chalk = require('chalk');
+import chalk from 'chalk';
 
-import { retryOperation } from '../'
-import { log } from '../../internals/logger';
+import { retryOperation } from '..';
+import log from '../../internals/logger';
 
 const _clickInSel = async (page: Page | Frame, sel: string) => {
   try {
@@ -21,16 +21,16 @@ const _clickInSel = async (page: Page | Frame, sel: string) => {
     log.debug(chalk`{red Cannot click in selector ${sel}}`);
     throw new Error(error);
   }
-}
+};
 
 const clickInSel = async (page: Page | Frame, sel: string) => {
   await retryOperation(async () => {
     try {
-      await _clickInSel(page, sel)
+      await _clickInSel(page, sel);
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
-  }, 3)
-}
+  }, 3);
+};
 
-export { clickInSel };
+export default clickInSel;
