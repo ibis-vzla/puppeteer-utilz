319d5d9ec86550b96c69bcf6a2e78e9146708583
Miguel Padrino <mikehathsin@gmail.com>
2020-03-26 04:26:45 -0400

    refactor: src/utils/retry-operation.ts

---
+++ src/utils/retry-operation.ts
@@ -0,0 +1,18 @@
+import { delay } from './';
+
+export const retryOperation = (operation: any, times: number) => (
+  new Promise((resolve, reject) => (
+    operation()
+      .then(resolve)
+      .catch((reason: any) => {
+        if (times - 1 > 0) {
+          return delay(100)
+            .then(retryOperation.bind(null, operation, times - 1))
+            .then(resolve)
+            .catch(reject);
+        }
+
+        reject(reason);
+      })
+  ))
+);
