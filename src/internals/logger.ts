7ccfec8bd3a6a3d9f53e856c2152588ea0750976
Miguel Padrino <mikehathsin@gmail.com>
2020-03-26 03:14:37 -0400

    refactor: internals/logger.ts

--- src/internals/logger.ts
+++ src/internals/logger.ts
@@ -1,6 +1,5 @@
 import { Signale } from 'signale';
 
-const signale = new Signale();
-const log = signale.scope('puppeteer-utilz');
-
-export default log;
+export const logger = new Signale({
+  scope: 'puppeteer-utilz'
+});
