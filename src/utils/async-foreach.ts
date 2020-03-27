644570e613e02e27ddf34b4671618db6c8483908
Miguel Padrino <mikehathsin@gmail.com>
2020-03-26 23:17:48 -0400

    refactor: async-foreach

--- src/utils/async-foreach.ts
+++ src/utils/async-foreach.ts
@@ -1,5 +1,5 @@
 export const asyncForeach = async (iterable: Array<any>, callback: any) => {
   for (let index = 0; index < iterable.length; ++index) {
-    await callback(iterable[index]);
+    await callback(iterable[index], index, iterable);
   }
 };
