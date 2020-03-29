a4158d81d6527151053c39584d6d5a220199bbfe
Miguel Padrino <mikehathsin@gmail.com>
2020-03-28 21:42:29 -0400

    add utils/noop

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export * from './async-foreach';
 export * from './click';
 export * from './delay';
 export * from './disable-logger';
+export * from './noop';
 export * from './retry';
 export * from './timeout';
 export * from './wait-url';
---
+++ src/utils/noop/index.ts
@@ -0,0 +1,5 @@
+import noop from './noop';
+
+export {
+  noop,
+};
