afb875b011295bf1eda95304faf179e10712ff41
Miguel Padrino <mikehathsin@gmail.com>
2020-04-04 23:46:08 -0400

    add find-frame reject test

--- __tests__/internals/find-frame/find-frame.test.ts
+++ __tests__/internals/find-frame/find-frame.test.ts
@@ -19,4 +19,19 @@ describe('with the imported find-frame module', () => {
 
     expect(frame?.name()).toBe(frameName);
   }, 30000);
+
+  it('must be possible: throw undefined if the frame is not found', async () => {
+    expect.hasAssertions();
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.banesconline.com/mantis/Website/Login.aspx');
+
+    const array = page.frames();
+    const frameName = 'test';
+    const frame = findFrame(array, frameName);
+    await browser.close();
+
+    expect(frame).toBe(undefined);
+  }, 30000);
 });
