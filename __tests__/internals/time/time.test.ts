ca43e7fbb33cb8ceb2fdb5cc91b99dc7f4cdc1d8
Miguel Padrino <mikehathsin@gmail.com>
2020-04-07 01:49:24 -0400

    add time tests

---
+++ __tests__/internals/time/time.test.ts
@@ -0,0 +1,19 @@
+import {
+  time,
+} from '../../../src/internals';
+
+describe('if a string is received', () => {
+  it('convert string to number', () => {
+    expect.hasAssertions();
+
+    expect(time('2 days')).toBe(172800000);
+  });
+});
+
+describe('if a number is received', () => {
+  it('do nothing', () => {
+    expect.hasAssertions();
+
+    expect(time(60000)).toBe(60000);
+  });
+});
