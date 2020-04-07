6157d57be590a69d7efcb8136e0a9201b6a6111c
Miguel Padrino <mikehathsin@gmail.com>
2020-04-07 01:27:08 -0400

    add logger test

---
+++ __tests__/internals/logger/logger.test.ts
@@ -0,0 +1,13 @@
+import {
+  logger,
+} from '../../../src/internals';
+
+describe('with the imported logger module', () => {
+  it('must be possible: send log messages', () => {
+    expect.hasAssertions();
+
+    logger.debug('test');
+
+    expect(logger.isEnabled()).toBe(true);
+  }, 30000);
+});
