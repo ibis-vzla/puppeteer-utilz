138fa221a951ecc3a40b75dc9525368aad18e016
Miguel Padrino <mikehathsin@gmail.com>
2020-04-08 06:56:20 -0400

    update wait-for-download test

--- __tests__/utils/wait-for-download/wait-for-download.test.ts
+++ __tests__/utils/wait-for-download/wait-for-download.test.ts
@@ -1,4 +1,9 @@
+import puppeteer from 'puppeteer';
+
 import {
+  delay,
+  findElement,
+  setDownloadPath,
   waitForDownload,
 } from '../../../src';
 
@@ -6,8 +11,21 @@ describe('with the imported wait-for-download module', () => {
   it('must be possible: wait for a download to complete', async () => {
     expect.hasAssertions();
 
-    const url = 'http://ovh.net/files/1Mb.dat';
-    const filename = await waitForDownload('~/test');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://ovh.net/files/');
+
+    const downloadPath = './download-path-test';
+    await setDownloadPath(page, {
+      behavior: 'allow',
+      downloadPath,
+    });
+    const element: any = await findElement(page, 'a', 'textContent', '1 Mbit file');
+    await element.click();
+    await delay('2 seconds');
+
+    const filename = await waitForDownload('./download-path-test');
+    await browser.close();
 
     expect(filename).toBe('1Mb.dat');
   }, 30000);
