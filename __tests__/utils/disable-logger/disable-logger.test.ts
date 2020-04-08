0ebd15354f354ae9c82ce6237c905810646a0baa
Miguel Padrino <mikehathsin@gmail.com>
2020-04-07 23:43:36 -0400

    add disable-logger test

---
+++ __tests__/utils/disable-logger/disable-logger.test.ts
@@ -0,0 +1,16 @@
+import {
+  disableLogger,
+} from '../../../src';
+import {
+  logger,
+} from '../../../src/internals';
+
+describe('with the imported disable-logger module', () => {
+  it('must be possible: disable the logger', () => {
+    expect.hasAssertions();
+
+    disableLogger();
+
+    expect(logger.isEnabled()).toBe(false);
+  }, 30000);
+});
