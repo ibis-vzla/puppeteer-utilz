f79f639b5e13fe7948c1c7ac0301f8b6182b6f96
Miguel Padrino <mikehathsin@gmail.com>
2020-04-12 19:53:00 -0400

    add timeout to the wait-for-url module avoiding infinite loops

--- __tests__/utils/wait-for-url/wait-for-url.test.ts
+++ __tests__/utils/wait-for-url/wait-for-url.test.ts
@@ -8,11 +8,26 @@ describe('with the imported wait-for-url module', () => {
   it('must be possible: wait for a redirect', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    page.goto('https://www.fb.com/');
+    await page.goto('https://www.fb.com/', {
+      timeout: 60000,
+    });
 
     const areURLsSame = await waitForURL(page, 'https://www.facebook.com/');
-    browser.close();
+    await browser.close();
 
     expect(areURLsSame).toBe(true);
-  });
+  }, 60000);
+
+  it(`shouldn't be possible to redirect in 100 milliseconds`, async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.fb.com/', {
+      timeout: 60000,
+    });
+
+    const areURLsSame = await waitForURL(page, 'https://www.facebook.com/', 100);
+    await browser.close();
+
+    expect(areURLsSame).toBe(false);
+  }, 60000);
 });
--- src/utils/wait-for-url/wait-for-url.ts
+++ src/utils/wait-for-url/wait-for-url.ts
@@ -3,20 +3,39 @@ import {
 } from 'puppeteer';
 
 import {
+  time,
+} from 'src/internals';
+import {
   delay,
 } from 'src/utils';
 
-const waitForURL = async (page: Page, expectedURL: string) => {
+const waitForURL = async (page: Page, expectedURL: string, timeout?: number | string) => {
+  if (timeout) {
+    timeout = time(timeout);
+  } else {
+    timeout = Infinity;
+  }
+
   let URLsAreDifferent = true;
+  let hrStart;
+  let hrEnd;
+  let timeElapsed = 0;
 
   while (URLsAreDifferent) {
-    const currentURL = page.url();
+    hrStart = process.hrtime();
 
+    const currentURL = page.url();
     if (currentURL === expectedURL) {
       URLsAreDifferent = false;
     }
-
     await delay(500);
+
+    hrEnd = process.hrtime(hrStart)[1] / 1000000;
+
+    timeElapsed += hrEnd;
+    if (timeElapsed > timeout) {
+      return false;
+    }
   }
 
   return true;
