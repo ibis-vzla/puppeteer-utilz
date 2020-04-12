99b0747b547bb2e3e2817633b3721a6c69c9021e
Miguel Padrino <mikehathsin@gmail.com>
2020-04-12 19:04:54 -0400

    add timeout to the wait-for-download module avoiding infinite loops

--- __tests__/utils/wait-for-download/wait-for-download.test.ts
+++ __tests__/utils/wait-for-download/wait-for-download.test.ts
@@ -14,7 +14,7 @@ describe('with the imported wait-for-download module', () => {
     const page = await browser.newPage();
     await page.goto('http://ovh.net/files/');
 
-    const downloadPath = './download-path-test';
+    const downloadPath = './1-test-folder';
     await setDownloadPath(page, {
       behavior: 'allow',
       downloadPath,
@@ -22,9 +22,30 @@ describe('with the imported wait-for-download module', () => {
     const element: any = await findElement(page, 'a', 'textContent', '1 Mbit file');
     await element.click();
 
-    const filename = await waitForDownload('./download-path-test');
+    const filename = await waitForDownload(downloadPath);
     await browser.close();
 
     expect(filename).toBe('1Mb.dat');
-  }, 30000);
+  }, 60000);
+
+  it(`shouldn't be possible to download 10 Gbit file in 1 second`, async () => {
+    expect.hasAssertions();
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://ovh.net/files/');
+
+    const downloadPath = './2-test-folder';
+    await setDownloadPath(page, {
+      behavior: 'allow',
+      downloadPath,
+    });
+    const element: any = await findElement(page, 'a', 'textContent', '10 Gbit file');
+    await element.click();
+
+    const filename = await waitForDownload(downloadPath, 1000);
+    await browser.close();
+
+    expect(filename).toBe(false);
+  }, 60000);
 });
--- src/utils/wait-for-download/wait-for-download.ts
+++ src/utils/wait-for-download/wait-for-download.ts
@@ -1,16 +1,32 @@
 import fs from 'fs';
 
 import {
-  delay,
-} from 'src/utils';
+  time,
+} from 'src/internals';
 
-const waitForDownload = async (path: string) => {
+const waitForDownload = async (path: string, timeout?: number | string) => {
+  if (timeout) {
+    timeout = time(timeout);
+  } else {
+    timeout = Infinity;
+  }
+
   let filename: string | undefined;
+  let hrStart;
+  let hrEnd;
+  let timeElapsed = 0;
 
   while (!filename || filename.endsWith('.crdownload')) {
-    await delay(1000);
+    while (!fs.existsSync(path));
 
+    hrStart = process.hrtime();
     filename = fs.readdirSync(path)[0];
+    hrEnd = process.hrtime(hrStart)[1] / 1000000;
+
+    timeElapsed += hrEnd;
+    if (timeElapsed > timeout) {
+      return false;
+    }
   }
 
   return filename;
